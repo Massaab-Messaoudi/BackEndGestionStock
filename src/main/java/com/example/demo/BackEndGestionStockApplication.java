069e1dcf58fb3e6ce26460b9553c0d76f56a52b8
Massaab <messaoudimassaab@gmail.com>
2020-06-18 13:07:52 +0200

    first commit

---
+++ src/main/java/com/example/demo/BackEndGestionStockApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndGestionStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndGestionStockApplication.class, args);
+	}
+
+}
